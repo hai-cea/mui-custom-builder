4d66ea4236012dc0c64900788c4fef27390fd942
Shaurya Arora <shaurya.arora1@gmail.com>
2015-12-10 16:37:14 -0600

    Add process.env.PORT for heroku

--- app.js
+++ app.js
@@ -89,7 +89,7 @@ app.get('/', function (req, res) {
 	}
 });
 
-var server = app.listen(3000, function () {
+var server = app.listen(process.env.PORT || 5000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
